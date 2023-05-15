11d53db7a2f0b6f4242859f3b003b2d2d0338212
Pratima Pore <100985414+Pratima4003@users.noreply.github.com>
2023-05-15 11:21:48 +0530

    Update heapsort.h

--- heapsortA/heapsort.h
+++ heapsortA/heapsort.h
@@ -1,17 +1,4 @@
 #include<stdlib.h>
-#define MAX 1
-#define MIN 0
-//structure of heap
-typedef struct heap{
-  int *A;
-  int s;
-  int rear;
-  int type;
-}heap;
-
-//functions
-void initH(heap *H, int type);
-void insertH(heap *H, int key);
-int deleteH(heap *H);
-void preorder(heap H);
-void heapSort(heap *h);
+#include<heap.h>
+//function
+void heapSort(heap *h, char order);
