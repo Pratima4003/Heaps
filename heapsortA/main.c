489c06c10763cd939f07186ffe040e9ff06f1f1a
Pratima Pore <100985414+Pratima4003@users.noreply.github.com>
2023-05-15 11:36:14 +0530

    Update main.c

--- heapsortA/main.c
+++ heapsortA/main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include"heap.h"
 #include"heapsort.h"
 
 int main(){
@@ -13,56 +14,13 @@ int main(){
     return 0;
   }
   initH(&H, flag);
-  insertH(&H, 12);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 1);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 13);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 121);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 98);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 17);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 15);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 45);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 3);
-  //preorder(H);
-  //printf("\n");
-  
-  insertH(&H, 23);
-  
-  insertH(&H, 43);
-  insertH(&H, 563);
-  insertH(&H, 27);
-  preorder(H);
-  printf("\n");
-  
-  printf("DELETED ELEMENT: %d\n",deleteH(&H));
-  preorder(H);
-  printf("\n");
-  heapSort(&H);
-  preorder(H);
-  printf("\n");
+  FILE *fp;
+  int n;
+  fp = fopen("coco.txt", "r");
+  for(int i = 0; i < 10; i++){
+    fscanf(fp, "%d", &n);
+    insertH(&H, n);
+  }
   return 0;
 }  
   
